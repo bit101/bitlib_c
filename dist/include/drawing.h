46d9fa3ac0aa10202bc63d4ee17c080dc1669e79
Keith Peters <kp@bit-101.com>
2020-05-08 20:33:55 -0400

    dist

---
+++ dist/include/drawing.h
@@ -0,0 +1,52 @@
+#ifndef _DRAWING_H_
+#define _DRAWING_H_
+
+#include <gtk/gtk.h>
+#include "point.h"
+#include "color.h"
+
+void cairo_set_source_color(cairo_t *cr, color *c);
+
+void cairo_clear_color(cairo_t *cr, color *c);
+void cairo_clear_rgb(cairo_t *cr, double r, double g, double b);
+void cairo_clear_rgba(cairo_t *cr, double r, double g, double b, double a);
+
+void cairo_plot(cairo_t *cr, double x, double y);
+
+void cairo_line(cairo_t *cr, double x0, double y0, double x1, double y1);
+void cairo_line_through(cairo_t *cr, double x0, double y0, double x1, double y1, double overlap);
+void cairo_ray(cairo_t *cr, double x, double y, double angle, double offset, double length);
+
+void cairo_stroke_rectangle(cairo_t *cr, double x, double y, double w, double h);
+void cairo_fill_rectangle(cairo_t *cr, double x, double y, double w, double h);
+void cairo_round_rectangle(cairo_t *cr, double x, double y, double w, double h, double r);
+void cairo_stroke_round_rectangle(cairo_t *cr, double x, double y, double w, double h, double r);
+void cairo_fill_round_rectangle(cairo_t *cr, double x, double y, double w, double h, double r);
+
+void cairo_circle(cairo_t *cr, double x, double y, double r);
+void cairo_stroke_circle(cairo_t *cr, double x, double y, double r);
+void cairo_fill_circle(cairo_t *cr, double x, double y, double r);
+
+void cairo_ellipse(cairo_t *cr, double x, double y, double xr, double yr);
+void cairo_stroke_ellipse(cairo_t *cr, double x, double y, double xr, double yr);
+void cairo_fill_ellipse(cairo_t *cr, double x, double y, double xr, double yr);
+
+void cairo_path(cairo_t *cr, point_list *path);
+void cairo_stroke_path(cairo_t *cr, point_list *path, int close);
+void cairo_fill_path(cairo_t *cr, point_list *path);
+
+void cairo_polygon(cairo_t *cr, double x, double y, double r, int sides, double rotation);
+void cairo_stroke_polygon(cairo_t *cr, double x, double y, double r, int sides, double rotation);
+void cairo_fill_polygon(cairo_t *cr, double x, double y, double r, int sides, double rotation);
+
+void cairo_star(cairo_t *cr, double x, double y, double r0, double r1, int points, double rotation);
+void cairo_stroke_star(cairo_t *cr, double x, double y, double r0, double r1, int points, double rotation);
+void cairo_fill_star(cairo_t *cr, double x, double y, double r0, double r1, int points, double rotation);
+
+void cairo_stroke_curve(cairo_t *cr, double x0, double y0, double x1, double y1, double x2, double y2);
+void cairo_quad_curve_to(cairo_t *cr, double x0, double y0, double x1, double y1);
+void cairo_stroke_quad_curve_to(cairo_t *cr, double x0, double y0, double x1, double y1);
+
+void cairo_fill_text(cairo_t *cr, char *text, double x, double y);
+void cairo_stroke_text(cairo_t *cr, char *text, double x, double y);
+#endif
--- makefile
+++ makefile
@@ -1,43 +1,55 @@
 CC := gcc
 SRC := src
 BUILD := build
+LIBS := libs
 TARGET := test/main.c
 BIN := bin
+DIST := dist
 EXE := $(BIN)/test
 INCLUDE := include
 CFLAGS := `pkg-config --cflags gtk+-3.0`
-LIBS := `pkg-config --libs gtk+-3.0`
+CLIBS := `pkg-config --libs gtk+-3.0`
 
 default: $(EXE)
 	@$(EXE)
 	@eog out.png
 
-$(EXE): $(BIN) $(BUILD) $(BUILD)/bitlib.a $(TARGET)
-	@$(CC) $(CFLAGS)  $(TARGET) -I$(INCLUDE) $(BUILD)/bitlib.a -o $(EXE) -lm $(LIBS)
+dist: $(LIBS) $(LIBS)/bitlib.a
+	@mkdir -p $(DIST)
+	@cp -r $(LIBS) $(DIST)
+	@cp -r $(INCLUDE) $(DIST)
+
+$(EXE): $(BIN) $(LIBS) $(LIBS)/bitlib.a $(TARGET)
+	@$(CC) $(CFLAGS)  $(TARGET) -I$(INCLUDE) $(LIBS)/bitlib.a -o $(EXE) -lm $(CLIBS)
 
 
-$(BUILD)/bitlib.a: $(BUILD)/drawing.o $(BUILD)/point.o $(BUILD)/perlin.o $(BUILD)/color.o
-	@ld -r $(BUILD)/drawing.o $(BUILD)/point.o $(BUILD)/perlin.o $(BUILD)/color.o -o $(BUILD)/bitlib.a
+$(LIBS)/bitlib.a: $(BUILD) $(BUILD)/drawing.o $(BUILD)/point.o $(BUILD)/perlin.o $(BUILD)/color.o
+	@ld -r $(BUILD)/drawing.o $(BUILD)/point.o $(BUILD)/perlin.o $(BUILD)/color.o -o $(LIBS)/bitlib.a
 
 $(BUILD)/drawing.o: $(SRC)/drawing.c
-	@$(CC) $(CFLAGS) -c $(SRC)/drawing.c -I$(INCLUDE) -o $(BUILD)/drawing.o -lm $(LIBS)
+	@$(CC) $(CFLAGS) -c $(SRC)/drawing.c -I$(INCLUDE) -o $(BUILD)/drawing.o -lm $(CLIBS)
 
 $(BUILD)/point.o: $(SRC)/point.c
-	@$(CC) $(CFLAGS) -c $(SRC)/point.c   -I$(INCLUDE) -o $(BUILD)/point.o   -lm $(LIBS)
+	@$(CC) $(CFLAGS) -c $(SRC)/point.c   -I$(INCLUDE) -o $(BUILD)/point.o   -lm $(CLIBS)
 
 $(BUILD)/perlin.o: $(SRC)/perlin.c
-	@$(CC) $(CFLAGS) -c $(SRC)/perlin.c  -I$(INCLUDE) -o $(BUILD)/perlin.o  -lm $(LIBS)
+	@$(CC) $(CFLAGS) -c $(SRC)/perlin.c  -I$(INCLUDE) -o $(BUILD)/perlin.o  -lm $(CLIBS)
 
 $(BUILD)/color.o: $(SRC)/color.c
-	@$(CC) $(CFLAGS) -c $(SRC)/color.c  -I$(INCLUDE) -o $(BUILD)/color.o  -lm $(LIBS)
+	@$(CC) $(CFLAGS) -c $(SRC)/color.c  -I$(INCLUDE) -o $(BUILD)/color.o  -lm $(CLIBS)
 
 $(BUILD):
 	@mkdir $(BUILD)
 
-bin:
+$(LIBS):
+	@mkdir $(LIBS)
+
+$(BIN):
 	@mkdir $(BIN)
 
 clean:
 	@rm -rf $(BUILD)
 	@rm -rf $(BIN)
-	@rm out.png
+	@rm -rf $(LIBS)
+	@rm -rf $(DIST)
+	@rm -f out.png

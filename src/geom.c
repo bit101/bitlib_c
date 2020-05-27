a16731125d073c17ea8b9740befb487540c14b76
Keith Peters <kp@bit-101.com>
2020-05-26 20:51:46 -0400

    beziers

--- src/geom.c
+++ src/geom.c
@@ -27,5 +27,21 @@ bool bl_circle_intersect(double cx0, double cy0, double cr0, double cx1,
   return sqrt(dx * dx + dy * dy) <= cr0 + cr1;
 }
 
+double bl_bezier(double x0, double x1, double x2, double x3, double t) {
+  double one_minus_t = 1.0 - t;
+  double m0 = one_minus_t * one_minus_t * one_minus_t;
+  double m1 = 3.0 * one_minus_t * one_minus_t * t;
+  double m2 = 3.0 * one_minus_t * t * t;
+  double m3 = t * t * t;
+  return m0 * x0 + m1 * x1 + m2 * x2 + m3 * x3;
+}
+
+double bl_quadratic_bezier(double x0, double x1, double x2, double t) {
+  double one_minus_t = 1.0 - t;
+  double m0 = one_minus_t * one_minus_t;
+  double m1 = 2.0 * one_minus_t * t;
+  double m2 = t * t;
+  return m0 * x0 + m1 * x1 + m2 * x2;
+}
 // xy in triangle
 // rect intersect
